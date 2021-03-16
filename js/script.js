bfc70392f7845126f4b139da007dac1045eec49f
Vcthtuf <Vcthtuf@gmail.com>
2021-03-16 22:31:18 +0500

    Added send data from form to server

--- js/script.js
+++ js/script.js
@@ -111,5 +111,41 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // ------------------------------------------------
 
+    // form 
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Мы с Вами скоро свяжемся',
+        failure: 'Что-то пошло не так'
+    }
+
+    let form = document.querySelector('.main-form'),
+        input = form.querySelectorAll('input'),
+        statusMessage = document.createElement('div');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        let formData = new FormData(form);
+        request.send(FormData);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status === 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+    });
+
+
+
 });
 
