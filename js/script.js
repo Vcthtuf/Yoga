4bdffd873f8a9ad55ac9f9dc45c47d23dc547f73
Vcthtuf <Vcthtuf@gmail.com>
2021-03-25 18:56:09 +0500

    Added calculator

--- js/script.js
+++ js/script.js
@@ -280,7 +280,46 @@ window.addEventListener('DOMContentLoaded', function () {
         daysSum = 0,
         total = 0;
 
-    totalValue.textContent = 0;
+    totalValue.innerHTML = 0;  // начальное значение суммы
+
+
+
+    persons.addEventListener('input', function () {  // обработка ввода числа людей
+        personSum = +this.value;
+        total = (personSum + daysSum) * 4000;       // вычисление суммы
+
+        console.log(persons.value);
+
+        if (restDays.value == '' || persons.value == '') {               // если поле "Дни" не заполнено
+            totalValue.innerHTML = 0;             // то не выводим результат
+        } else {                                  // иначе
+            totalValue.innerHTML = total;         // выводим результат
+        }
+    });
+
+    restDays.addEventListener('input', function () {    // обработка ввода числа дней
+        daysSum = +this.value;
+        total = (personSum + daysSum) * 4000;      // вычисление суммы
+
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function () {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
+
+
+
+
 
 
 
